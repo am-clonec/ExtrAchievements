1008f5e4909b5106bfd4dec3a9e36f53592b6363
am-clonec <s.brouwer111@gmail.com>
2026-06-19 14:56:00 +0200

    Created project

---
+++ ExtrAchievements/ExtrAchievementsPlugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using Reactor.Utilities;
+
+namespace ExtrAchievements;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class ExtrAchievementsPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+    public ConfigEntry<string> ConfigName { get; private set; }
+
+    public override void Load()
+    {
+        ConfigName = Config.Bind("Fake", "Name", ":>");
+
+        Harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
+    public static class ExamplePatch
+    {
+        public static void Postfix(PlayerControl __instance)
+        {
+            __instance.cosmetics.nameText.text = PluginSingleton<ExtrAchievementsPlugin>.Instance.ConfigName.Value;
+        }
+    }
+}
